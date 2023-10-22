5e7b2cbe5a0c258f27baaf89caab6a9c6ece8d0f
Ravindra Kadam <65077652+ravi2320@users.noreply.github.com>
2023-10-23 00:56:43 +0530

    94. Bianry Tree Inorder Traversal
    https://leetcode.com/problems/binary-tree-inorder-traversal/solutions/4196973/cpp-easy-solution-recursion-100-beats/
    
    The algorithm starts at the root node of the binary tree. It then recursively visits the left subtree of the root node. As each node in the left subtree is visited, its value is added to the vector. Once the left subtree has been visited, the algorithm visits the root node and adds its value to the vector. The algorithm then recursively visits the right subtree of the root node. As each node in the right subtree is visited, its value is added to the vector. Once the right subtree has been visited, the algorithm returns the vector.

---
+++ Day_6_94_Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,64 @@
+/*
+
+
+Code
+
+Testcase
+Testcase
+Result
+
+94. Binary Tree Inorder Traversal
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& arr) 
+    {
+        if (root == NULL) return;
+
+        inorder(root->left, arr);
+        arr.push_back(root->val);
+        inorder(root->right, arr);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> arr;
+        inorder(root, arr);
+        return arr;
+    }
+};
