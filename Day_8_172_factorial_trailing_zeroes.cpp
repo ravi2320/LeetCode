094da455b7d38fa66e83390fd796ee341c04f100
Ravindra Kadam <65077652+ravi2320@users.noreply.github.com>
2023-10-26 00:06:40 +0530

    Day_8_172_factorial_trailing_zeroes

---
+++ Day_8_172_factorial_trailing_zeroes.cpp
@@ -0,0 +1,47 @@
+/*
+
+172. Factorial Trailing Zeroes
+Solved
+Medium
+Topics
+Companies
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+Example 2:
+
+Input: n = 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+Example 3:
+
+Input: n = 0
+Output: 0
+ 
+
+Constraints:
+
+0 <= n <= 104
+
+*/
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int cnt=0;
+        while(n/5>0){
+            cnt=cnt+n/5;
+            n=n/5;
+        }
+
+        return cnt;
+    }
+};
