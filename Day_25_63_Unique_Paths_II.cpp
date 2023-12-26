f5aaf692d87df910c76b77bf2a485aa74f89bab4
Ravindra Kadam <65077652+ravi2320@users.noreply.github.com>
2023-12-26 22:04:51 +0530

    63. Unique Paths II

---
+++ Day_25_63_Unique_Paths_II.cpp
@@ -0,0 +1,83 @@
+/*
+63. Unique Paths II
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+
+Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The testcases are generated so that the answer will be less than or equal to 2 * 109.
+
+ 
+
+Example 1:
+
+
+Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+Output: 2
+Explanation: There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
+Example 2:
+
+
+Input: obstacleGrid = [[0,1],[0,0]]
+Output: 1
+ 
+
+Constraints:
+
+m == obstacleGrid.length
+n == obstacleGrid[i].length
+1 <= m, n <= 100
+obstacleGrid[i][j] is 0 or 1.
+*/
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1)
+            return 0;
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        // Initialize the top-left cell
+        dp[0][0] = 1;
+
+        // Initialize the first row
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 1)
+                break;
+            dp[i][0] = dp[i - 1][0];
+        }
+
+        // Initialize the first column
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[0][j] == 1)
+                break;
+            dp[0][j] = dp[0][j - 1];
+        }
+
+        // Fill the dp array
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1)
+                    dp[i][j] = 0; // If there is an obstacle, no paths through this cell
+                else
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+};
