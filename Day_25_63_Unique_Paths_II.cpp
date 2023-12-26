017c7bc63e65cfc993a7c6a8afd227e1f65e88fb
Ravindra Kadam <65077652+ravi2320@users.noreply.github.com>
2023-12-26 22:24:03 +0530

    Day_25_63_Unique_Paths_II.cpp

--- Day_25_63_Unique_Paths_II.cpp
+++ Day_25_63_Unique_Paths_II.cpp
@@ -71,9 +71,7 @@ public:
         // Fill the dp array
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                if (obstacleGrid[i][j] == 1)
-                    dp[i][j] = 0; // If there is an obstacle, no paths through this cell
-                else
+                if (obstacleGrid[i][j] == 0)
                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
